7731709fc4836320702388f5e209831c716be779
σ`∀´)σ <hitdoomed@gmail.com>
2021-01-07 16:35:06 +0800

    Create Caesar_cipher.py

---
+++ Caesar_cipher.py
@@ -0,0 +1,23 @@
+a = input()
+
+for i in range(26):
+    print('Displacement '+str(i)+':')
+    for j in range(len(a)):
+        ans = ord(a[j])
+        if 96 < ans < 123:
+            if ans+i > 123:
+                ans = ans - 26
+
+            c = chr(ans+i)
+
+            print(c,end='')
+        elif 64 < ans < 91:
+            if ans+i > 90:
+                ans = ans - 26
+
+            c = chr(ans+i)
+
+            print(c,end='')
+        else:
+            print(a[j],end='')
+    print('\n')
